ddf5b3523614bba1fe4f1c75d74bc5577db74618
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-18 12:01:47 +0200

    180422 navBar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './home.css';
+import bg from '../../images/frontpagebg.png';
+import logo from '../../images/airbnb.png';
+import { ConnectButton } from 'web3uikit';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+        <div className='containerGradient'></div>
+      </div>
+      <div className='topBanner'>
+        <div>
+          <img className='logo' src= {logo} alt='logo' />
+        </div>
+        <div className='tabs'>
+          <div className='selected'>Places To Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div className='lrContainers'>
+          <ConnectButton />
+        </div>
+      </div>
+    </>
   )
 }
 
