084fff53d585bab1c9f286d29bbb70124774e815
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-19 18:12:33 +0200

    190422 rentals searcher

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import './home.css';
 import bg from '../../images/frontpagebg.png';
 import logo from '../../images/airbnb.png';
-import { ConnectButton, Select, DatePicker } from 'web3uikit';
+import { ConnectButton, Select, DatePicker, Input, Icon, Button } from 'web3uikit';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [destination, setDestination] = useState('New York');
+  const [guests, setGuests] = useState(2);
+
+
+
   return (
     <>
       <div className="container" style={{ backgroundImage: `url(${bg})` }}>
@@ -28,7 +38,7 @@ const Home = () => {
           <div className='inputs'>Location
           <Select
             defaultOptionIndex={0}
-            onChange={null}
+            onChange={(data) => {setDestination(data.label)}}
             options={[
               {
               id: 'ny',
@@ -53,14 +63,54 @@ const Home = () => {
           <div className='inputs'>Check in
             <DatePicker
               id="date-picker"
-              onChange={null}
+              onChange={(event) => {setCheckIn(event.date)}}
             />
           </div>
           <div className='vl' />
-          <div className='inputs'>Check out</div>
+          <div className='inputs'>Check out
+            <DatePicker
+              id="date-picker"
+              onChange={(event) => setCheckOut(event.date)}
+            />
+          </div>
           <div className='vl' />
-          <div className='inputs'>Guests</div>
+          <div className='inputs'>Guests
+            <Input
+              value={2}
+              name="AddGuests"
+              onChange={(event) => setGuests(event.target.value)}
+              type="number"
+            />
+          </div>
+
+          {/* The search button is inlcuded into the link to
+          another page */}
+          <Link to={'/rentals'} state={{
+            destination: destination,
+            checkIn: checkIn,
+            checkOut: checkOut,
+            guests: guests,
+          }}>
+            <div className='searchButton'>
+              <Icon
+                fill="#ffffff"
+                size={24}
+                svg="search"
+              />
+            </div>
+          </Link>
         </div>
+      </div>
+      <div className="randomLocation">
+        <div className="title">Feel Adventurous</div>
+        <div className="text">
+          Let us decide and discover new places to stay, live, work or just
+          relax.
+        </div>
+        <Button
+          text="Explore A Location"
+          onClick={() => console.log(checkOut)}
+        />
       </div>
     </>
   )
--- src/pages/rentals/Rentals.jsx
+++ src/pages/rentals/Rentals.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import './rentals.css';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import logo from '../../images/airbnbRed.png'
+import { ConnectButton, Icon } from 'web3uikit';
 
 const Rentals = () => {
+  const {state: searchFilters} = useLocation();
+
   return (
-    <div>Rentals</div>
+    <>
+     <div className='topBanner'>
+        <div>
+          <Link to='/'>
+            <img className='logo' src= {logo} alt='logo' />
+          </Link>
+        </div>
+        <div className='searchReminder'>
+        <div className='searchFields'>
+          <div className='filter'>{searchFilters.destination}</div>
+          <div className='vl' />
+          <div className='filter'>
+            {`
+            ${searchFilters.checkIn.toLocaleString('default', {month: 'short',})}
+            ${searchFilters.checkIn.toLocaleString('default', {day: '2-digit',})}
+            `}
+            -
+            {`
+            ${searchFilters.checkOut.toLocaleString('default', {month: 'short',})}
+            ${searchFilters.checkOut.toLocaleString('default', {day: '2-digit',})}
+            `}
+          </div>
+          <div className='vl' />
+          <div className='filter'>
+            {searchFilters.guests} Guest
+          </div>
+          <div className='searchButton'>
+              <Icon
+                fill="#ffffff"
+                size={24}
+                svg="search"
+              />
+            </div>
+      </div>
+        </div>
+        {/* Wallet */}
+        <div className='lrContainers'>
+          <ConnectButton />
+        </div>
+      </div>
+    </>
+
   )
-}
+} 
 
 export default Rentals;
