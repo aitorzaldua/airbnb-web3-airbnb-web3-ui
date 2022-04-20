4972de176d746ff1efc876667ffc2b16a52719d4
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-20 12:15:17 +0200

    200422 adding googlemaps

---
+++ src/components/RentalsMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RentalsMap ({locations}) {
+  return (
+      <>
+        <div>RentalsMap</div>
+      </>
+  );
+}
+
+export default RentalsMap;
--- src/pages/rentals/Rentals.jsx
+++ src/pages/rentals/Rentals.jsx
@@ -3,10 +3,32 @@ import './rentals.css';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import logo from '../../images/airbnbRed.png'
-import { ConnectButton, Icon } from 'web3uikit';
+import { Button, ConnectButton, Icon } from 'web3uikit';
+import RentalsMap from '../../components/RentalsMap';
 
 const Rentals = () => {
   const {state: searchFilters} = useLocation();
+
+  const rentalsList = [
+    {
+      attributes: {
+        city: "New York",
+        unoDescription: "3 Guests • 2 Beds • 2 Rooms",
+        dosDescription: "Wifi • Kitchen • Living Area",
+        imgUrl:
+          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
+        lat: "40.716862",
+        long: "-73.999005",
+        name: "Apartment in China Town",
+        pricePerDay: "3",
+      },
+    },
+  ];
+
+  let cords = [];
+  rentalsList.forEach((e) => {
+    cords.push({ lat: e.attributes.lat, lng: e.attributes.long });
+  });
 
   return (
     <>
@@ -47,6 +69,46 @@ const Rentals = () => {
         {/* Wallet */}
         <div className='lrContainers'>
           <ConnectButton />
+        </div>
+      </div>
+
+      {/* Content for the rentals page */}
+      <hr className='line' />
+      <div className='rentalsContent'>
+        <div className='rentalsContentL'>
+          Stays Available for Your Destination
+          {rentalsList &&
+          rentalsList.map(e => {
+            return(
+              <>
+                <hr className='line2' />
+                <div className='rentalDiv'>
+                  <img className='rentalImg' src={e.attributes.imgUrl} alt='img_rental'></img>
+                  <div className='rentalInfo'>
+                    <div className='rentalTitle'>{e.attributes.name}</div>
+                    <div className='rentalDesc'>
+                      {e.attributes.unoDescription}
+                    </div>
+                    <div className='rentalDesc'>
+                      {e.attributes.dosDescription}
+                    </div>
+                    <div className='bottomButton'>
+                      <Button
+                        text='Stay Here'
+                      />
+                    <div className='price'>
+                      <Icon fill='#808080' size={10} svg='matic' />{e.attributes.pricePerDay} / Day
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
+        </div>
+        <hr className='rentalDiv'></hr>
+        <div className='rentalsContentR'>
+          <RentalsMap locations={cords}/>
         </div>
       </div>
     </>
